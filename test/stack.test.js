b73dd9278475864dda0fb00610c4e18892958071
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-25 23:03:21 +0530

    add the test file with testing pop, push and initialize methods.

---
+++ test/stack.test.js
@@ -0,0 +1,44 @@
+class Stack{
+    constructor() {
+        this.top = -1
+        this.items = {}
+    }
+    get peek (){
+        return this.items[this.top]
+    }
+    push(value) {
+        this.top += 1
+        this.items[this.top] = value
+    }
+    pop(){
+        const item = this.items[this.top]
+        delete this.items[this.top]
+        this.top -= 1
+        return item
+    }
+}
+describe("My Stack", () => {
+    let stack;
+    beforeEach(()=>{
+        stack = new Stack()
+    })
+    it("is created empty.", () => {
+        expect(stack.top).toBe(-1)
+        expect(stack.items).toEqual({})
+    }) // it === test
+    // it.todo === tod*o
+    it("can push to the top", ()=>{
+        stack.push("🎡")
+        expect(stack.top).toBe(0)
+        expect(stack.peek).toBe("🎡")
+    })
+    it("can pop off", ()=>{
+        stack.push("🎡")
+        let item = stack.pop()
+
+        expect(stack.top).toBe(-1)
+        expect(stack.peek).toEqual(undefined)
+        expect(stack.items).toEqual({})
+        expect(item).toBe("🎡")
+    })
+})
